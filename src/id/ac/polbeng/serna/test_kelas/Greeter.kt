9e2f9a9376344a9dba73e4934b81828aa4fe41b8
sernawty013-boop <sernawty013@gmail.com>
2025-09-22 10:24:09 +0700

    ex1 Materi OOP di kotlin

---
+++ src/id/ac/polbeng/serna/test_kelas/Greeter.kt
@@ -0,0 +1,13 @@
+package id.ac.polbeng.serna.test_kelas
+class Greeter {
+    var text: String = ""
+    fun greet() {
+        println("Hello object world!")
+    }
+    fun greet(name: String) {
+        println("$text $name")
+    }
+    fun with_ret_val(name: String): String {
+        return "$text $name"
+    }
+}
---
+++ src/id/ac/polbeng/serna/test_kelas/GreeterTest.kt
@@ -0,0 +1,10 @@
+package id.ac.polbeng.serna.test_kelas
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Anton")
+    greeter.greet("Budi")
+    greeter.text = "Hello programmer"
+    println(greeter.with_ret_val("Dono"))
+}
